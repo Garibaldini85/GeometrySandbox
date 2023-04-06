f7fac116517bebe4b16e3e1c886b8acf9f3f7ae1
Garibaldini85 <garibaldini27185285@gmail.com>
2023-04-06 02:21:34 +0200

    The FGeometryPayload structure has been created.
    Added a setters for the components of the FGeometryPayload structure.
    Added the ability to create a list of FGeometryPayload objects.

--- Source/GeometrySandbox/Private/GeometryHubActor.cpp
+++ Source/GeometrySandbox/Private/GeometryHubActor.cpp
@@ -9,13 +9,44 @@ AGeometryHubActor::AGeometryHubActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AGeometryHubActor::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UWorld* World = AActor::GetWorld();
+	if (World)
+	{
+		for(uint8 i = 0; i < 10; i++)
+		{
+			GeometryPayload.SetGeometryData(
+				50, 2.0f + i,
+				EMovementType::Sin, FLinearColor::Black, NULL);
+
+			GeometryPayload.SetColorChangerData(
+				10000 + 100 * i, NULL, NULL,
+				{}, false);
+
+			GeometryPayload.SetTransform(FRotator::ZeroRotator, FVector(0.0f, 300.0f + i * 100, 300.0f));
+
+			ABaseGeometryActor* Geometry = World->SpawnActor<ABaseGeometryActor>(GeometryPayload.GeometryClass, GeometryPayload.Transform);
+			Geometry->SetColorChangerData(GeometryPayload.ColorChangerData);
+			Geometry->SetGeometryData(GeometryPayload.GeometryData);
+			
+			Geometry->FinishSpawning(GeometryPayload.Transform);
+		}
+	}
+
+	for (const auto elem: GeometryPayloads)
+	{
+		ABaseGeometryActor* Geometry = World->SpawnActor<ABaseGeometryActor>(elem.GeometryClass, elem.Transform);
+		Geometry->SetGeometryData(elem.GeometryData);
+		Geometry->SetColorChangerData(elem.ColorChangerData);
+			
+		Geometry->FinishSpawning(elem.Transform);
+	}
 }
 
 // Called every frame
--- Source/GeometrySandbox/Public/GeometryHubActor.h
+++ Source/GeometrySandbox/Public/GeometryHubActor.h
@@ -4,7 +4,55 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "BaseGeometryActor.h"
+
 #include "GeometryHubActor.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGeometryPayload
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<ABaseGeometryActor> GeometryClass;
+
+	UPROPERTY(EditAnywhere)
+	FGeometryData GeometryData;
+
+	UPROPERTY(EditAnywhere)
+	FColorChangerData ColorChangerData;
+
+	UPROPERTY(EditAnywhere)
+	FTransform Transform;
+
+	void SetGeometryData(const float& Amplitude = NULL, const float& Frequency = NULL,
+	    EMovementType MoveType = EMovementType::Undefined, const FLinearColor& Color = FLinearColor::Black,
+		const float& TimerRate = NULL)
+	{
+		if (Amplitude != NULL) 	GeometryData.Amplitude = Amplitude;
+		if (Frequency != NULL)	GeometryData.Frequency = Frequency;
+		if (TimerRate != NULL)	GeometryData.TimerRate = TimerRate;
+		if (Color     != FLinearColor::Black)		GeometryData.Color     = Color;
+		if (MoveType  != EMovementType::Undefined)	GeometryData.MoveType  = MoveType;
+	}
+
+	void SetColorChangerData(const int& Interval = NULL, const float& Step = NULL,
+		const float& CurrentStep = NULL, const TArray<uint32>& ArraIntColor= {},
+		const bool& IsLogging = false)
+	{
+		if (Interval != NULL) 		 ColorChangerData.Interval    = Interval;
+		if (Step != NULL)			 ColorChangerData.Step        = Step;
+		if (CurrentStep != NULL)	 ColorChangerData.CurrentStep = CurrentStep;
+		if (IsLogging  != false)	 ColorChangerData.IsLogging     = IsLogging;
+		if (ArraIntColor.Num() != 0) ColorChangerData.ArrayIntColor = ArraIntColor;
+	}
+
+	void SetTransform(const FRotator& Rotator, const FVector& Vector)
+	{
+		Transform.SetRotation(Rotator.Quaternion());
+		Transform.SetLocation(Vector);
+	}
+};
 
 UCLASS()
 class GEOMETRYSANDBOX_API AGeometryHubActor : public AActor
@@ -18,7 +66,13 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	
+	UPROPERTY(EditAnywhere, Category = "Geometry Payload Data")
+	FGeometryPayload GeometryPayload;
+	
+	UPROPERTY(EditAnywhere, Category = "Geometry Payload Data")
+	TArray<FGeometryPayload> GeometryPayloads;
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
